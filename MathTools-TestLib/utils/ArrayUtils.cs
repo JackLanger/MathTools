433edeeaab15e2ddaaa54693a3a04883a5f5fb96
Langer, Jacek <jacek.langer@tu-darmstadt.de>
2023-01-27 14:10:14 +0100

    chore: added test utilities

---
+++ MathTools-TestLib/utils/ArrayUtils.cs
@@ -0,0 +1,25 @@
+﻿namespace MathTools_TestLib.utils;
+
+public static class ArrayUtils
+{
+    public static bool ArrayAreEqual(double[] actual, double[] expected)
+    {
+        for (var i = 0; i < actual.Length; i++)
+            if (!NumberIsEqual(actual[i], expected[i]))
+                return false;
+
+        return true;
+    }
+
+    /// <summary>
+    ///     Checks if two numbers are equal, by defining a certain accuracy.
+    /// </summary>
+    /// <param name="actual">actual</param>
+    /// <param name="expected">the expected Value</param>
+    /// <param name="accuracy">Range of accuracy defaults to 0.00001 due to floating point errors</param>
+    /// <returns>true if actual equal +- accuracy</returns>
+    public static bool NumberIsEqual(double actual, double expected, double accuracy = 0.00001)
+    {
+        return Math.Abs(actual - expected) < accuracy;
+    }
+}
